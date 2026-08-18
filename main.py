5360d78e921b312c6c1703307e73984b5695f2fd
AmeerKCloud <ameer.khan.linkedin@gmail.com>
2026-08-18 13:35:33 -0400

    updates

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-url = "https://data.cityofnewyork.us/api/v3/views/erm2-nwe9/query.csv"
+url = "https://data.cityofnewyork.us/api/v3/views/erm2-nwe9/query.csv" #⬅️ Currently working here.
 import pandas as pd
 data = pd.read_csv(url)
 print(data.head())
