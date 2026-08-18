3eb54a8f83d2df9c6b92a3854aacb7476a811a50
AmeerKCloud <ameer.khan.linkedin@gmail.com>
2026-08-18 13:46:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-url = "https://data.cityofnewyork.us/api/v3/views/erm2-nwe9/query.csv?$where=created_date between '2024-01-01T00:00:00' and '2024-12-31T23:59:59'&borough='BROOKLYN'&$limit=500000"" #⬅️ Currently working here.
+url = "https://data.cityofnewyork.us/api/v3/views/erm2-nwe9/query.csv?$where=created_date between '2024-01-01T00:00:00' and '2024-12-31T23:59:59'&borough='BROOKLYN'&$limit=500000"
 import pandas as pd
 data = pd.read_csv(url)
 print(data.head())
