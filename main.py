f919ccdc7cf299ee6469baffe141a45cb7b0af19
AmeerKCloud <ameer.khan.linkedin@gmail.com>
2026-08-19 13:10:22 -0400

    updates

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from urllib.parse import quote
+from urllib.parse import quote  #⬅️ quote() from Python's urllib.parse converts spaces to %20 and safely encodes special characters.
 #
 base = "https://data.cityofnewyork.us/resource/erm2-nwe9.csv"
 where_clause = "created_date between '2024-01-01T00:00:00' and '2024-12-31T23:59:59' AND borough='BROOKLYN'"
