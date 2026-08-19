8f9637e5076854404eb76238b6a47f0f23af93e1
Ameer Khan <ameer.khan.linkedin@gmail.com>
2026-08-18 20:01:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pandas as pd
 from urllib.parse import quote
-
-base = "https://data.cityofnewyork.us/api/v3/views/erm2-nwe9/query.csv"
+#
+base = "https://data.cityofnewyork.us/resource/erm2-nwe9.csv"
 where_clause = "created_date between '2024-01-01T00:00:00' and '2024-12-31T23:59:59' AND borough='BROOKLYN'"
-url = f"{base}?$where={quote(where_clause)}&$limit=500000"
+url = f"{base}?$where={quote(where_clause)}&$limit=5000"
+#
+# data = pd.read_csv(url)
+# print(data)
 
+print("Starting request...")
 data = pd.read_csv(url)
-print(data)
+print("Done!")
+print(data.head())
