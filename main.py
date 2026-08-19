207352514a7fca90e54ddebbcfb34b23eabd2c0c
AmeerKCloud <ameer.khan.linkedin@gmail.com>
2026-08-19 13:21:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import pandas as pd
 from urllib.parse import quote  #⬅️ [1] 'quote()' from Python's urllib.parse converts spaces to %20 and safely encodes special characters.
 #
-base = "https://data.cityofnewyork.us/resource/erm2-nwe9.csv"   #⬅️ API with complete dataset.
+base = "https://data.cityofnewyork.us/resource/erm2-nwe9.csv"   #⬅️ SODA2 API with complete dataset.
 
 where_clause = "created_date between '2024-01-01T00:00:00' and '2024-12-31T23:59:59' AND borough='BROOKLYN'"    #⬅️ Filter requirements.
 
 url = f"{base}?$where={quote(where_clause)}&$limit=5000"
-#
+
 # data = pd.read_csv(url)
 # print(data)
 
