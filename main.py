ad2ff52a83ccd79e86838d42ec79a6ef10528080
Ameer Khan <ameer.khan.linkedin@gmail.com>
2026-08-20 20:58:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ print("Done!")                      #⬅️ Test to ensure python script is runn
 
 # print(df.head())                  #⬅️ '.head()' method retrieves & displays first few rows.
 
-print(type(df.columns))         #⬅️ '.columns' is an attribute that returns an Index object (a special pandas data structure) containing all the column labels of the DataFrame.
+# print(type(df.columns))         #⬅️ '.columns' is an attribute that returns an Index object (a special pandas data structure) containing all the column labels of the DataFrame.
 
-# column_headings = df.columns.tolist()    #⬅️'.tolist()' converts a pandas index obj. or series data-type into python list.
+# column_headings = df.columns.tolist()    #⬅️'.tolist()' converts a pandas index obj. or series data-structure into python list.
 
 # for heading in column_headings:
 #     print(heading)
